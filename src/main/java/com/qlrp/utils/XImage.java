b8883dc052138742002eb2b228a8e281a3215df5
Khanh <hoangkhanh2420@gmail.com>
2022-07-26 02:17:01 +0700

    Update UI QLHOME
    cập nhật giao diên tạm thời

--- src/main/java/com/qlrp/utils/XImage.java
+++ src/main/java/com/qlrp/utils/XImage.java
@@ -19,7 +19,7 @@ import javax.swing.ImageIcon;
  */
 public class XImage {
     public static Image getAppIcon() {
-        URL url = XImage.class.getResource("/com/qlbh/icon/Sys/Icon_app.png");
+        URL url = XImage.class.getResource("/com/qlrp/icon/Sys/Icon_app.png");
         return new ImageIcon(url).getImage();
     }
     
